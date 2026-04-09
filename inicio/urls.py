e4d47ae38c61ede20058e5dc2dfa0cb104ac18e3
Cristian D Biancotti <cdbiancotti@gmail.com>
2026-04-08 22:04:43 -0300

    Manejo de templates, datos en templates y herencia de templates

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from inicio.views import inicio
+from inicio.views import inicio, prueba_bucle
 
 app_name = 'inicio'
 
 urlpatterns = [
-    path('', inicio, name='inicio')
+    path('', inicio, name='inicio'),
+    path('prueba-bucle/', prueba_bucle, name='prueba_bucle'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# v1
+# def inicio(request):
+#     return HttpResponse("<h1>Hola Mundo</h1>")
 
+# v2
 def inicio(request):
-    return HttpResponse("Hola Mundo")
+    
+    # return render(request, 'inicio.html', {})
+    return render(request, 'inicio/inicio.html')
+
+def prueba_bucle(request):
+
+    numeros = list(range(1, 11))
+
+    return render(request, 'inicio/prueba_bucle.html', {'datos': numeros})
